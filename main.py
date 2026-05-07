5346295756a2bd31e70ccb745991967d020d30af
brunoalmeida-art <bruno.almeida@roldao.com.br>
2026-05-07 13:59:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,28 +4,35 @@ url = "https://services-beta.vipcommerce.com.br/api-admin/v1/org/67/filial/1/cen
 
 headers = {
 
-    "User-Agent": "Mozilla/5.0",
+    "user-agent": "Mozilla/5.0",
 
-    "Accept": "application/json",
+    "accept": "application/json",
 
-    "Content-Type": "application/json",
+    "content-type": "application/json",
 
     "organizationid": "67",
 
-    "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJ2aXBjb21tZXJjZSIsImF1ZCI6ImFwaS1hZG1pbiIsInN1YiI6IjZiYzQ4NjdlLWRjYTktMTFlOS04NzQyLTAyMGQ3OTM1OWNhMCIsInZpcGNvbW1lcmNlQ2xpZW50ZUlkIjpudWxsLCJpYXQiOjE3NzI3MTEyNDMsInZlciI6MSwiY2xpZW50IjpudWxsLCJvcGVyYXRvciI6bnVsbCwib3JnIjoiNjcifQ.5jbsro83AZ-4AG5jJsZKrbgeyocPa6n1vUQclalIR_HgF5FaxEFhJIcC0dggPwzdBzV0nFgPBJkk6ABFH6tDkQ"
+    "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJ2aXBjb21tZXJjZSIsImF1ZCI6ImFwaS1hZG1pbiIsInN1YiI6IjZiYzQ4NjdlLWRjYTktMTFlOS04NzQyLTAyMGQ3OTM1OWNhMCIsInZpcGNvbW1lcmNlQ2xpZW50ZUlkIjpudWxsLCJpYXQiOjE3NzI3MTEyNDMsInZlciI6MSwiY2xpZW50IjpudWxsLCJvcGVyYXRvciI6bnVsbCwib3JnIjoiNjcifQ.5jbsro83AZ-4AG5jJsZKrbgeyocPa6n1vUQclalIR_HgF5FaxEFhJIcC0dggPwzdBzV0nFgPBJkk6ABFH6tDkQ"
 
 }
 
 print("TESTANDO API SPANI...")
 
-response = requests.get(
-    url,
-    headers=headers,
-    timeout=30
-)
+try:
 
-print("STATUS:")
-print(response.status_code)
+    response = requests.get(
+        url,
+        headers=headers,
+        timeout=30
+    )
 
-print("RESPOSTA:")
-print(response.text)
+    print("STATUS:")
+    print(response.status_code)
+
+    print("RESPOSTA:")
+    print(response.text)
+
+except Exception as e:
+
+    print("ERRO:")
+    print(e)
