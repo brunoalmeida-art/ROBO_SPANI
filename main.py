30268e95082cd4516ac21169730f070d8f0b74df
brunoalmeida-art <bruno.almeida@roldao.com.br>
2026-05-07 12:32:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import requests
 url = "https://services-beta.vipcommerce.com.br/api-admin/v1/org/67/filial/1/centro_distribuicao/36/loja/produtos/823/detalhes"
 
 headers = {
+
     "User-Agent": "Mozilla/5.0",
-    "Accept": "application/json"
+
+    "Accept": "application/json",
+
+    "organizationid": "67",
+
+    "origin": "https://www.spanionline.com.br",
+
+    "referer": "https://www.spanionline.com.br/"
+
 }
 
 print("TESTANDO API SPANI...")
