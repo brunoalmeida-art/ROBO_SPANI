f0fc774b8859d440cdd35e15ed22c1847d63ce1d
brunoalmeida-art <bruno.almeida@roldao.com.br>
2026-05-07 15:12:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ linhas = []
 with sync_playwright() as p:
 
     browser = p.chromium.launch(
-        headless=False,
+        headless=True,
         args=[
             "--disable-blink-features=AutomationControlled"
         ]
