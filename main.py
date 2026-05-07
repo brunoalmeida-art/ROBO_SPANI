196e5ddbc6ed0cfb7144b60dc4714ee7084485d8
brunoalmeida-art <bruno.almeida@roldao.com.br>
2026-05-07 12:44:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,31 +8,24 @@ headers = {
 
     "Accept": "application/json",
 
+    "Content-Type": "application/json",
+
     "organizationid": "67",
 
-    "origin": "https://www.spanionline.com.br",
-
-    "referer": "https://www.spanionline.com.br/"
+    "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJ2aXBjb21tZXJjZSIsImF1ZCI6ImFwaS1hZG1pbiIsInN1YiI6IjZiYzQ4NjdlLWRjYTktMTFlOS04NzQyLTAyMGQ3OTM1OWNhMCIsInZpcGNvbW1lcmNlQ2xpZW50ZUlkIjpudWxsLCJpYXQiOjE3NzI3MTEyNDMsInZlciI6MSwiY2xpZW50IjpudWxsLCJvcGVyYXRvciI6bnVsbCwib3JnIjoiNjcifQ.5jbsro83AZ-4AG5jJsZKrbgeyocPa6n1vUQclalIR_HgF5FaxEFhJIcC0dggPwzdBzV0nFgPBJkk6ABFH6tDkQ"
 
 }
 
 print("TESTANDO API SPANI...")
 
-try:
+response = requests.get(
+    url,
+    headers=headers,
+    timeout=30
+)
 
-    response = requests.get(
-        url,
-        headers=headers,
-        timeout=30
-    )
+print("STATUS:")
+print(response.status_code)
 
-    print("STATUS:")
-    print(response.status_code)
-
-    print("RESPOSTA:")
-    print(response.text)
-
-except Exception as e:
-
-    print("ERRO:")
-    print(e)
+print("RESPOSTA:")
+print(response.text)
