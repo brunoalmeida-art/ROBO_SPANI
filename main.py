e4f7880806aa34a99f8f6f1f7de5f036800c3dbc
brunoalmeida-art <bruno.almeida@roldao.com.br>
2026-05-07 12:14:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("ROBO SPANI INICIADO")
+import requests
+
+url = "https://services-beta.vipcommerce.com.br/api-admin/v1/org/67/filial/1/centro_distribuicao/36/loja/produtos/823/detalhes"
+
+headers = {
+    "User-Agent": "Mozilla/5.0",
+    "Accept": "application/json"
+}
+
+print("TESTANDO API SPANI...")
+
+try:
+
+    response = requests.get(
+        url,
+        headers=headers,
+        timeout=30
+    )
+
+    print("STATUS:")
+    print(response.status_code)
+
+    print("RESPOSTA:")
+    print(response.text)
+
+except Exception as e:
+
+    print("ERRO:")
+    print(e)
