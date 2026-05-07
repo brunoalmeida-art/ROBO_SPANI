e1eda005c819f56bdebb61ffe6c5bafe9e7b74a4
brunoalmeida-art <bruno.almeida@roldao.com.br>
2026-05-07 15:19:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,37 +12,37 @@ with sync_playwright() as p:
     )
 
     context = browser.new_context(
-        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36",
-        viewport={"width": 1920, "height": 1080},
-        locale="pt-BR"
+        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36"
     )
 
     page = context.new_page()
 
-    # remove webdriver flag
-    page.add_init_script("""
-        Object.defineProperty(navigator, 'webdriver', {
-            get: () => undefined
-        })
-    """)
+    # =========================
+    # MONITORA REQUESTS
+    # =========================
 
-    url = "https://www.spanionline.com.br/produto/832/leite-longa-vida-italac-1l-semidesnatado"
+    def capturar(response):
+
+        url = response.url
+
+        if "api" in url.lower():
+
+            print("\n====================")
+            print("API:")
+            print(url)
+
+    page.on("response", capturar)
+
+    # =========================
+    # ABRE PRODUTO
+    # =========================
 
     page.goto(
-        url,
+        "https://www.spanionline.com.br/produto/832/leite-longa-vida-italac-1l-semidesnatado",
         wait_until="networkidle",
         timeout=120000
     )
 
-    page.wait_for_timeout(10000)
-
-    print("URL FINAL:")
-    print(page.url)
-
-    print("\nTITULO:")
-    print(page.title())
-
-    print("\nHTML INICIO:")
-    print(page.content()[:5000])
+    page.wait_for_timeout(15000)
 
     browser.close()
